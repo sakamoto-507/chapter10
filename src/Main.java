9e302421ad98c0da681b7864d2757ccb963733d1
SakamotoChisato <241127@std.hi-joho.ac.jp>
2025-06-13 11:33:34 +0900

    6/13

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        SuperHero sh = new SuperHero();
-        /*Hero h = new Hero();
+        /*SuperHero sh = new SuperHero();
+        Hero h = new Hero();
         h.name = "ミナト";
         h.run();
         sh.name = "スーパーミナト";
         sh.fly();
         sh.run();
         sh.test();*/
+        String name1 = "ななしの剣";
+        int num1 = 10;
+        String name2 = "剣2";
+        int num2 = 15;
+        Weapon w1 = new Weapon(name1,num1);
+        Weapon w2 = new Weapon(name2,num2);
+        System.out.println(w1.name + "\n" + "￥" + w1.price);
+        System.out.println(w2.name + "\n" + "￥" + w2.price);
     }
 }
---
+++ src/Weapon.java
@@ -0,0 +1,5 @@
+public class Weapon extends Item {
+    public Weapon(String a,int b) {
+        super(a,b); //引数1つの親クラスコンストラクタを呼び出す。
+    }
+}
